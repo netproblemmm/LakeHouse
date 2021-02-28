493b7fb309f720133316eeb74014fa0f935e9baa
netproblemmm <ubhawk1@mail.ru>
2021-03-01 02:20:35 +0300

    HW_4 Creating animations

---
+++ Assets/Scripts/Soldier_sounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Soldier_sounds : MonoBehaviour
+{
+    [SerializeField] private AudioSource _footStep;
+    [SerializeField] private AudioSource _gunShot;
+
+    public void PlayFootStep()
+    {
+        _footStep.Play();
+    }
+
+    public void PlayGunShot()
+    {
+        _gunShot.Play();
+    }
+}
