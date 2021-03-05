f0c27eaaf8fcc4b380b7247faa082b23c49513a3
netproblemmm <ubhawk1@mail.ru>
2021-03-05 20:44:21 +0300

    HW_5 Creating IK Animation and Ragdoll

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/IKAnimation.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animatorGO;
+    [SerializeField] private Transform handObj;
+    [SerializeField] private Transform lookObj;
+    [SerializeField] private Transform rightFoot;
+    [SerializeField] private Transform leftFoot;
+    [SerializeField] private Transform head;
+
+    [SerializeField] private bool ikActive;
+    [SerializeField] private float rightHandWeight = 1;
+    [SerializeField] private float leftHandWeight = 1;
+
+    [SerializeField] private float lookObjDistance;
+
+    void Start()
+    {
+        animatorGO = GetComponent <Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (ikActive)
+        {
+            if (handObj)
+            {
+                animatorGO.SetIKPositionWeight(AvatarIKGoal.RightHand, rightHandWeight);
+                animatorGO.SetIKRotationWeight(AvatarIKGoal.RightHand, rightHandWeight);
+
+                animatorGO.SetIKPosition(AvatarIKGoal.RightHand, handObj.position);
+                animatorGO.SetIKRotation(AvatarIKGoal.RightHand, handObj.rotation);
+
+                animatorGO.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftHandWeight);
+                animatorGO.SetIKRotationWeight(AvatarIKGoal.LeftHand, leftHandWeight);
+
+                animatorGO.SetIKPosition(AvatarIKGoal.LeftHand, handObj.position);
+                animatorGO.SetIKRotation(AvatarIKGoal.LeftHand, handObj.rotation);
+            }
+
+            if (lookObj)
+            {
+                lookObjDistance = Vector3.Distance(lookObj.position, head.position);
+                if (lookObjDistance <= 2)
+                {
+                    animatorGO.SetLookAtWeight(1);
+                }
+                else
+                {
+                    animatorGO.SetLookAtWeight(0);
+                }
+                animatorGO.SetLookAtPosition(lookObj.position);
+            }
+        }
+        else
+        {
+            animatorGO.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+            animatorGO.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+            animatorGO.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+            animatorGO.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+            animatorGO.SetLookAtWeight(0);
+        }
+    }
+}
---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Ragdoll.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    [SerializeField] private Rigidbody RB;
+    [SerializeField] private float killForce = 5f;
+    [SerializeField] private bool kill;
+    [SerializeField] private bool revive;
+
+    [SerializeField] private Rigidbody[] RBsArray;
+
+    [SerializeField] private Collider[] colls;
+    [SerializeField] private Animator anim;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        RBsArray = GetComponentsInChildren<Rigidbody>();
+        colls = GetComponentsInChildren<Collider>();
+        Revive();
+    }
+
+    private void Kill()
+    {
+        kill = false;
+        SetRagdoll(true);
+        SetMain(false);
+    }
+
+    private void Revive()
+    {
+        revive = false;
+        SetRagdoll(false);
+        SetMain(true);
+    }
+
+    void SetRagdoll(bool active)
+    {
+        for (int i = 0; i < RBsArray.Length; i++)
+        {
+            RBsArray[i].isKinematic = !active;
+        }
+
+        for (int i = 0; i < colls.Length; i++)
+        {
+            colls[i].enabled = active;
+        }
+    }
+
+    void SetMain(bool active)
+    {
+        anim.enabled = active;
+        RBsArray[0].isKinematic = !active;
+        colls[0].enabled = active;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Kill();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Revive();
+        }
+    }
+}
